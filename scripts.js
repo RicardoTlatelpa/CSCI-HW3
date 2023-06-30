713fb96125aa56ff855c66dcaee3e6a1cf3972e4
Ricardo Tlatelpa <ricardotlatelpa@Ricardos-MacBook-Pro.local>
2023-06-30 16:40:11 -0400

    added calculator logic

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 var dotFlag = false;
 var inputs = [];
+var last_operator = '';
+var lock = true;
 
 function queueLength(){
     return inputs.length;
@@ -9,6 +11,7 @@ function getInput(){
     return document.getElementById('calculatorInput');
 }
 var calculatorInput = getInput();
+
 function resetInput(){
     calculatorInput.value = "0";
 }
@@ -23,10 +26,16 @@ function resetDot(){
     }    
 }
 
+function appendInput(number){    
+    if(queueLength() != 2){             
+        inputs.push(number);
+    }
+}
 
 resetInput();
 
 function appendToResult(value){
+    lock = false;
     if (calculatorInput && calculatorInput.value == "0"){
         calculatorInput.value = value;
     }
@@ -36,14 +45,15 @@ function appendToResult(value){
 }
 
 function clearResult(){
-    if (calculatorInput.value != "0"){
-        resetInput();        
-        resetDot();
-        inputs = [];
-    }    
+    
+    resetInput();        
+    resetDot();
+    inputs = [];
+    lock = true
 }
 
 function performOperation(n1,n2,operator){
+    console.log(n1,n2,operator)
     if(operator == "+"){
         return n1 + n2;
     }
@@ -63,14 +73,22 @@ function performOperation(n1,n2,operator){
 
 function handleOperation(operator){ // two states can come here
     const LENGTH = queueLength();
-    let newInput = 0;
-    if(LENGTH == 2){
-        newInput = performOperation(inputs.shift(),inputs.shift(), operator);
+    if(operator == '+' || operator == '-' || operator == '/' || operator == '%' || operator == '*'){
+        if(!lock){
+            lock = true;
+        }
+        last_operator = operator;
     }
-    else if(LENGTH == 1){
-        newInput = performOperation(inputs.shift(), 0, operator);
+    let newInput = false;
+    if(LENGTH > 0 && operator == "="){
+      let n1 = inputs.shift();
+      let n2 = inputs.shift();
+      newInput = performOperation(n1,n2, last_operator);      
+    }    
+    console.log(newInput)
+    if(newInput != false && !isNaN(newInput)){
+      setInput(newInput);
     }
-    setInput(newInput)
 }
 
 function handleOperator(operator){
@@ -78,14 +96,23 @@ function handleOperator(operator){
         if(!dotFlag){
             dotFlag = true;
             if (getInput().value == "0"){
-                appendToResult("0.")
+                appendToResult("0.");
+            }
+            else {
+              appendToResult(".");
             }
         }
     }
-    else if(operator == '+'){
-        inputs.push(Number(getInput.value));
+    else{
+      if(!lock){
+        appendInput(parseInt(getInput().value));        
+      }
+      
+      console.log(inputs)
+      if(getInput().value != "0"){
         resetInput();
         resetDot();
-        handleOperation(operator);
+      }      
+      handleOperation(operator);
     }
 }
